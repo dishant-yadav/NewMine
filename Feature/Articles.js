f54a02c91ea3537b759f5f33812a1f2d61ad2927
“[Dishant <[dishantyadav599@gmail.com]>
2022-09-04 23:44:28 +0530

    "changes"

---
+++ Feature/Articles.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Articles = (props) => {
+  return <article>
+    This is a artcile
+  </article>;
+};
+
+Articles.propTypes = {};
+
+export default Articles;
